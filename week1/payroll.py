1018be2bd96c56aad25070e7efb6d05947bf4071
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-11-18 17:42:31 -0800

    11/18/21

--- week1/payroll.py
+++ week1/payroll.py
@@ -1,14 +1,18 @@
 class Employee():
-    # def __init__(self, fName, lName, employeeId, hourlyWage):
-    #     super(Employee, self).__init__(fName, lName, employeeId, hourlyWage)
-
-    fName = str(input("Please Enter The Employee's First Name: "))
-    lName = str(input("Please Enter The Employee's Last Name: "))
-    employeeId = int(input("Please Enter The Employee's ID number: "))
-    hourlyWage = float(input("Please Enter The Employee's Hourly Wage: "))
-    
-    def pay(self, hoursWorked):
-        paycheck = hoursWorked * self.hourlyWage
-        paycheck.type = float
+    def pay():
+        employeeId = int(input("Please Enter The Employee's ID number: "))
+        fName = str(input("Please Enter The Employee's First Name: "))
+        lName = str(input("Please Enter The Employee's Last Name: "))
+        hourlyWage = float(input("Please Enter The Employee's Hourly Wage: "))
+        hoursWorked = float(
+            input("How many hours did " + fName + " " + lName + " work this week? "))
+        paycheck = str(hoursWorked * hourlyWage)
         if(hoursWorked <= 40.0):
-            return paycheck
+            print(fName + " " + lName + "'s paycheck amount is $" + paycheck)
+        elif(hoursWorked > 40.0):
+            timeAndAHalf = hoursWorked - 40.0
+            hourlyWage = hourlyWage * 1.5
+            overtime = timeAndAHalf * hourlyWage
+            bonusPay = str(paycheck + overtime)
+            print(fName + " " + lName + "'s paycheck amount is $" + bonusPay)
+Employee.pay()
