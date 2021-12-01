8ae9d2cfd542e92cbc2e014c6bf94c99f0752f2e
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-12-01 11:38:30 -0800

    12/1/21

--- week2/assignment_two/dataStructures.py
+++ week2/assignment_two/dataStructures.py
@@ -1,103 +1,44 @@
-from random import randint
-# Data1
-print("\nData1")
-data_one = (7, False, "Apple", True, 7, 98.6)
+# Tasks:
+
+# Data 1
+Data1 = (7, False, "Apple", True, 7, 98.6)
+# Data1 - Count the number of items
+print(len(Data1))
+# Data1 - Find the value of the fourth item stored in the data set
+print(Data1[3])
+# Data1 - Count the number of times 7 appears
+print(Data1.count(7))
 
 
-def count():
-    count = 0
-    for i in data_one:
-        count += 1
-    print("\t", count)
+# Data 2
+Data2 = {"July", 4, 8, "Tango", 4.3, "Bingo"}
+# Data2 - Remove a random element from the data set
+Data2.pop()
+# Data2 - Add "Alpha" to the data set
+Data2.add("Alpha")
+# Data2 - Print data set
+print(Data2)
 
 
-count()
+# Data 3
+Data3 = ["A", 7, -1, 3.14, True, 7]
+# Data3 - Print the data set in reverse order
+Data3.reverse()
+print(Data3)
+# Data3 - Change the second value in the data set to ‘B’
+Data3[1] = "B"
+# Data3 - Remove the last item and print the data set
+Data3.pop()
+print(Data3)
 
 
-def findFourthItem():
-    count = 0
-    for i in data_one:
-        count += 1
-        if count == 4:
-            print("\t", i)
-
-
-findFourthItem()
-
-
-def countSeven():
-    count = 0
-    for i in data_one:
-        if i == 7:
-            count += 1
-    print("\t", count)
-
-
-countSeven()
-
-# Data2
-print("\nData2")
-data_two = {"July", 4, 8, "Tango", 4.3, "Bingo"}
-
-
-def removeRand():
-    count = 0
-    for i in data_two:
-        count += 1
-        value = randint(0, count)
-        if count == value:
-            data_two.remove(i)
-            data_two.add("Alpha")
-            break
-        else:
-            value += 1
-    print("\t", data_two)
-
-
-removeRand()
-
-# Data3
-print("\nData3")
-data_three = ["A", 7, -1, 3.14, True, 7]
-
-
-def reverseOrder():
-    data_three.reverse()
-    data_three.pop()
-    count = 0
-    for i in data_three:
-        count += 1
-        if count == 2:
-            data_three[1] = "B"
-    print("\t", data_three)
-
-
-reverseOrder()
-
-# Data4
-print("\nData4")
-data_four = {"name": "Joe", "age": 26, "active": True, "hourly_wage": 14.75}
-
-
-def changeActive():
-    data_four.update({"active": False})
-
-
-changeActive()
-
-
-def joePaycheck():
-    hourlyWage = data_four.get("hourly_wage")
-    paycheck = (hourlyWage * 40)
-    print("\t", paycheck)
-
-
-joePaycheck()
-
-
-def addAddress():
-    data_four.update({"address": "123 West Main Street"})
-    print("\t", data_four)
-
-
-addAddress()
+# Data 4
+Data4 = {"name": "Joe",  "age": 26,   "active": True,  "hourly_wage": 14.75}
+# Data4 - Change "active" to False
+Data4.update({"active": False})
+# Data4 - Add "address" with a value of "123 West Main Street"
+Data4.update({"address": "123 West Main Street"})
+# Data4 - Print the weekly salary for Joe if he worked a full 40 hour week.
+print(Data4.get("hourly_wage")*40)
+# Data4 - Print the data set
+print(Data4)
