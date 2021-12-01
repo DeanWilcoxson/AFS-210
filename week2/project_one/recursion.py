392271f7c1c102907db727b72a81a8e9927d2b2d
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-11-30 16:04:15 -0800

    11/30/21

--- week2/project_one/recursion.py
+++ week2/project_one/recursion.py
@@ -7,7 +7,10 @@ def loop1():
             odd_sum += i
     print(odd_sum)
     return odd_sum
+
+
 loop1()
+
 
 def loop2():
     # Sum the even numbers between 1 and 20
@@ -19,24 +22,31 @@ def loop2():
         i += 1
     print(even_sum)
     return even_sum
+
+
 loop2()
 
 # Recursion
+
+
 def loop1Rec(num=20, odd_sum=0):
     if num <= 1:
         odd_sum += num
         return odd_sum
-    else:
-        if num % 2 == 1:
-            odd_sum += num
-        return loop1Rec(num-1, odd_sum)
+    elif num % 2 == 1:
+        odd_sum += num
+    return loop1Rec(num-1, odd_sum)
+
+
 print(loop1Rec())
+
 
 def loop2Rec(num=0, even_sum=0):
     if num >= 20:
         return even_sum
-    else:
-        if num % 2 == 0:
-            even_sum += num
-        return loop2Rec(num+1, even_sum)
+    elif num % 2 == 0:
+        even_sum += num
+    return loop2Rec(num+1, even_sum)
+
+
 print(loop2Rec())
