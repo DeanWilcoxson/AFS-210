a6baf48cfaf9120e7c13147475379cde2f0e7084
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-12-09 00:37:59 -0800

    12/8/21

--- week3/linked_lists/doubly-linked.py
+++ week3/linked_lists/doubly-linked.py
@@ -1,90 +1,73 @@
 class Node:
-    # A doubly-linked node.
     def __init__(self, data=None):
         self.data = data
         self.next = None
         self.prev = None
-
 class DoublyLinkedList:
-    # A doubly-linked list.
     def __init__(self):
-        # Create an empty list.
         self.tail = None
         self.head = None
         self.count = 0
 
     def iter(self):
-        # Iterate through the list.
         current = self.head
         while current:
             val = current.data
             current = current.next
             yield val
 
-
     def size(self) -> int:
-        # Returns the number of elements in the list
         return self.count
 
-
     def addFirst(self, data) -> None:
-        # Add a node at the front of the list
-
+        node = Node(data)
+        node.next = self.head
+        self.head.prev = node
+        self.head = node
+        node.prev = None
+        self.count += 1
 
     def addLast(self, data) -> None:
-        # Add a node at the end of the list
-  
+        node = Node(data)
+        self.tail.prev = node
+        self.tail = node
+        node.next = None
+        self.count += 1
 
-    def addAtIndex(self, data, index):
-        # Add a node to the list at the given index position
-        # If index equals to the length of linked list, the node will be appended to the end of linked list
-        # If index is greater than the length, the data will not be inserted.
-        # This function does not replace the data at the index, but pushes everything else down.
-
-
-    def indexOf(self, data):
-        # Search through the list. Return the index position if data is found, otherwise return -1    
-
-
+    # def addAtIndex(self, data, index):
+        node = Node(data)
+        
+    # def indexOf(self, data):
 
     def add(self, data) -> None:
         # Append an item to the end of the list
         self.addLast(data)
 
     def clear(self) -> None:
-        # Remove all of the items from the list
         self.head = None
         self.tail = None
         self.count = 0
 
     def deleteAtIndex(self, index) -> None:
-        # Delete the node at the index-th in the linked list, if the index is valid.
-
         if (index > (self.count-1)):
             return
-            
         curr = self.head
         prev = self.head
-
         for n in range(index):
             prev = curr
             curr = curr.next
-            
         prev.next = curr.next
         curr.prev = prev
         self.count -= 1
-
         if (curr == self.head):
             self.head = curr.next
             curr.prev = None
         elif (curr == self.tail):
             prev.next = None
-            self.tail = prev       
-
+            self.tail = prev
         return
 
     def delete(self, data) -> None:
-        # Delete a node from the list who's value matches the supplied value
         current = self.head
         prev = self.head
         while current:
@@ -122,5 +105,5 @@ class DoublyLinkedList:
     def __str__(self):
         myStr = ""
         for node in self.iter():
-             myStr += str(node)+ " "
+            myStr += str(node) + " "
         return myStr
