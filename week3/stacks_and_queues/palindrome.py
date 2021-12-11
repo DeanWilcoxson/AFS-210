772d89869c3f9319e411fadb7d421975f62f7f80
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-12-11 01:31:57 -0800

    12/11/21

---
+++ week3/stacks_and_queues/palindrome.py
@@ -0,0 +1,80 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class Stack:
+    def __init__(self):
+        self.top = None
+        self.size = 0
+
+    def push(self, data):
+        node = Node(data)
+        if self.top:
+            node.next = self.top
+            self.top = node
+        else:
+            self.top = node
+        self.size += 1
+
+    def pop(self):
+        if self.top:
+            data = self.top.data
+            self.size -= 1
+            if self.top.next:
+                self.top = self.top.next
+            else:
+                self.top = None
+            return data
+        else:
+            return None
+
+    def getSize(self):
+        return self.size
+
+    def isEmpty(self):
+        if(self.size == 0):
+            return True
+        else:
+            return False
+
+    def peek(self):
+        if self.top:
+            return self.top.data
+        else:
+            return None
+
+class Queue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.count = 0
+
+    def enqueue(self, data):
+        new_node = Node(data, None, None)
+        if self.head is None:
+            self.head = new_node
+            self.tail = self.head
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+
+        self.count += 1
+
+    def dequeue(self):
+        current = self.head
+        if self.count == 1:
+            self.count -= 1
+            self.head = None
+            self.tail = None
+        elif self.count > 1:
+            self.head = self.head.next
+            self.head.prev = None
+            self.count -= 1
+        
+# def isPalindrome(str):
+# racecar = isPalindrome("racecar")
+# noon = isPalindrome("noon")
+# python = isPalindrome("python")
+# madam = isPalindrome("madam")
