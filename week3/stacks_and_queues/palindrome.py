e6c7bddebd5f0bf24cb8d79fcb30488e1bc7659e
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-12-12 13:03:00 -0800

    12/12/21

--- week3/stacks_and_queues/palindrome.py
+++ week3/stacks_and_queues/palindrome.py
@@ -1,101 +1,68 @@
-class Node:
-    def __init__(self, data=None):
-        self.data = data
-        self.next = None
-
 class Stack:
     def __init__(self):
-        self.top = None
-        self.size = 0
+        self.stack = []
 
     def push(self, data):
-        node = Node(data)
-        if self.top:
-            node.next = self.top
-            self.top = node
-        else:
-            self.top = node
-        self.size += 1
+        self.stack.insert(0, data)
+        return
 
     def pop(self):
-        if self.top:
-            data = self.top.data
-            self.size -= 1
-            if self.top.next:
-                self.top = self.top.next
-            else:
-                self.top = None
-            return data
-        else:
-            return None
+        return self.stack.pop(0)
 
     def getSize(self):
-        return self.size
+        return len(self.stack)
 
     def isEmpty(self):
-        if(self.size == 0):
+        if(len(self.stack) <= 0):
             return True
         else:
             return False
 
     def peek(self):
-        if self.top:
-            return self.top.data
-        else:
-            return None
+        return self.stack[0]
 
 class Queue:
     def __init__(self):
-        self.head = None
-        self.tail = None
-        self.count = 0
+        self.queue = []
 
     def enqueue(self, data):
-        new_node = Node(data, None, None)
-        if self.head is None:
-            self.head = new_node
-            self.tail = self.head
-        else:
-            new_node.prev = self.tail
-            self.tail.next = new_node
-            self.tail = new_node
-
-        self.count += 1
+        self.queue.insert(0, data)
 
     def dequeue(self):
-        current = self.head
-        if self.count == 1:
-            self.count -= 1
-            self.head = None
-            self.tail = None
-        elif self.count > 1:
-            self.head = self.head.next
-            self.head.prev = None
-            self.count -= 1
+        data = self.queue.pop()
+        return data
 
     def getSize(self):
-        return self.size
+        return len(self.queue)
 
     def isEmpty(self):
-        if(self.size == 0):
+        if(len(self.queue) == 0):
             return True
         else:
             return False
 
     def peek(self):
-        if self.top:
-            return self.top.data
+        if self.head:
+            return self.head.data
         else:
             return None
 
 def isPalindrome(str):
-    if(str == str[::-1]):
-        print(True)
-    else:
-        print(False)
-    
-    
-racecar = isPalindrome("racecar")
-noon = isPalindrome("noon")
-python = isPalindrome("python")
-madam = isPalindrome("madam")
+    stack = Stack()
+    queue = Queue()
+    for ch in str:
+        stack.push(ch)
+        queue.enqueue(ch)
+        if(stack.isEmpty() != True):
+            for x in stack.stack:
+                stackLetter = stack.pop()
+                queueLetter = queue.dequeue()
+                if(stackLetter == queueLetter):
+                    return True
+                else:
+                    return False
+
+print(isPalindrome("racecar"))
+print(isPalindrome("noon"))
+print(isPalindrome("python"))
+print(isPalindrome("madam"))
