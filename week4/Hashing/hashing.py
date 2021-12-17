4dc4df409e292b4a895cbcb353e0eb95b30bec55
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-12-16 16:50:57 -0800

    12/16/21

--- week4/Hashing/hashing.py
+++ week4/Hashing/hashing.py
@@ -2,8 +2,7 @@ class HashItem:
     def __init__(self, key, value):
         self.key = key
         self.value = value
-
-
+        
 class HashTable:
     def __init__(self):
         self.size = 10
@@ -40,9 +39,16 @@ class HashTable:
         if self.slots[hash_key] is None:
             self.count += 1
         self.slots[hash_key] = item
-        return    
-    # def get(self, key):
+        return
+
+    def get(self, key):
         # Insert your code here to get data by key
+        hash_key = self._hash(key)
+        while self.slots[hash_key] is not None:
+            if self.slots[hash_key].key is key:
+                return self.slots[hash_key].value
+            hash_key = (hash_key + 1) % self.size
+        return None
 
     def __getitem__(self, key):
         return self.get(key)
@@ -50,10 +56,9 @@ class HashTable:
     def __setitem__(self, key, value):
         self.put(key, value)
 
-
 H = HashTable()
 H[69] = 'A'
-print(H._hash("John"))
+
 # Store remaining input data
 
 # print the slot values
