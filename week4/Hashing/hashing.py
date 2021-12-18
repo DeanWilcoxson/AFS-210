84bed7ce7bcc0c532da3586b737a3c2886060511
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-12-18 13:32:19 -0800

    12/16/21

--- week4/Hashing/hashing.py
+++ week4/Hashing/hashing.py
@@ -1,75 +1,61 @@
-class HashItem:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
 class HashTable:
     def __init__(self) -> None:
         self.size = 10
         self.slots = [None] * self.size
-        self.count = 0
-        # self.data = [None] * self.size
+        self.data = [None] * self.size
+
+    def keys(self):
+        return self.slots
+
+    def values(self):
+        return self.data
 
     def _hash(self, key):
-        # Insert your hashing function code
         return key % self.size
 
     def _rehash(self, key):
-        # Insert your secondary hashing function code
         return key // self.size
 
     def put(self, key, value):
-        # Insert your code here to store key and data
-        item = HashItem(key, value)
-        hash_key = self._hash(key)
-        while self.slots[hash_key] is not None:
-            if self.slots[hash_key].key is key:
-                break
-            hash_key = (hash_key + 1) % self.size
-        if self.slots[hash_key] is None:
-            self.count += 1
-        self.slots[hash_key] = item
-        print(item)
-        return 
-
-    def get(self, key):
-        # Insert your code here to get data by key
-        hash_key = self._hash(key)
-        while self.slots[hash_key] is not None:
-            if self.slots[hash_key].key is key:
-                return self.slots[hash_key].value
-            hash_key = (hash_key + 1) % self.size
-        return None
-
-    def __getitem__(self, key):
-        return self.get(key)
+        h_value = self._hash(key)
+        if (self.data[h_value] == None):
+            self.data[h_value] = value
+            self.slots[h_value] = key
+        else:
+            if(self.slots[h_value] == key):
+                self.data[h_value] = value
+            else:
+                h_value = self._rehash(key)
+                if(self.data[h_value] == None):
+                    self.data[h_value] = value
+                    self.slots[h_value] = key
+                else:
+                    return None
 
     def __setitem__(self, key, value):
         self.put(key, value)
 
+    def get(self, key):
+        h_value = self._hash(key)
+        return self.data[h_value]
+
+    def __getitem__(self, key):
+        return self.get(key)
+
 # Store remaining input data
 H = HashTable()
-H.__setitem__(69, 'A')
-H.__setitem__(66, 'B')
-H.__setitem__(80, 'C')
-H.__setitem__(35, 'D')
-H.__setitem__(18, 'E')
-H.__setitem__(52, 'F')
-H.__setitem__(89, 'G')
-H.__setitem__(70, 'H')
-H.__setitem__(12, 'I')
-
-
+H[69] = 'A'
+H[66] = 'B'
+H[80] = 'C'
+H[35] = 'D'
+H[18] = 'E'
+H[52] = 'F'
+H[89] = 'G'
+H[70] = 'H'
+H[12] = 'I'
 # print the slot values
-
+print(H.keys())
+# print the data values
+print(H.values())
 # print the value for key 52
-print(H.__getitem__(69))
-print(H._hash(69))
-print(H.__getitem__(66))
-print(H.__getitem__(80))
-print(H.__getitem__(35))
-print(H.__getitem__(18))
-print(H.__getitem__(52))
-print(H.__getitem__(89))
-print(H.__getitem__(70))
-print(H.__getitem__(12))
-
+print(H[52])
