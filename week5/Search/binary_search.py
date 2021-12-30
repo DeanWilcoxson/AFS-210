293f2127f0e6d35c0759ddda1a7e521cd3d4c6c2
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-12-29 16:40:17 -0800

    12/29/21

--- week5/Search/binary_search.py
+++ week5/Search/binary_search.py
@@ -1,4 +1,4 @@
-def binary_search_recursive(list, value, start=0, end=None):
+def recursive_BS(list, value, start=0, end=None):
     if end is None:
         end = len(list) - 1
     if start > end:
@@ -8,12 +8,12 @@ def binary_search_recursive(list, value, start=0, end=None):
     if value == list[mid]:
         return True
     if value < list[mid]:
-        return binary_search_recursive(list, value, start, mid-1)
+        return recursive_BS(list, value, start, mid-1)
     # value > list[mid]
-    return binary_search_recursive(list, value, mid+1, end)
+    return recursive_BS(list, value, mid+1, end)
 
 
-def binary_search_iterative(list, value):
+def iterative_BS(list, value):
     start, end = 0, (len(list) - 1)
     while start <= end:
         mid = (start + end) // 2
@@ -31,10 +31,10 @@ sampleInput1 = [7, 20, 26, 31, 40, 51, 55, 63, 74, 81]
 sampleInput2 = [7, 20, 26, 31, 40, 51, 55, 63, 74, 81]
 sampleInput3 = ["Alpha", "Beta", "Delta", "Epsilon", "Gamma", "Theta", "Zeta"]
 
-# print(binary_search_recursive(sampleInput1, 31))
-# print(binary_search_recursive(sampleInput2, 77))
-# print(binary_search_recursive(sampleInput3, "Delta"))
+# print(recursive_BS(sampleInput1, 31))
+# print(recursive_BS(sampleInput2, 77))
+# print(recursive_BS(sampleInput3, "Delta"))
 
-print(binary_search_iterative(sampleInput1, 31))
-print(binary_search_iterative(sampleInput2, 77))
-print(binary_search_iterative(sampleInput3, "Delta"))
+print(iterative_BS(sampleInput1, 31))
+print(iterative_BS(sampleInput2, 77))
+print(iterative_BS(sampleInput3, "Delta"))
