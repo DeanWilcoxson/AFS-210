b91c6ab171f6843c66de33f16c23a224ab88f513
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-12-29 16:38:51 -0800

    12/29/21

--- week5/Search/binary_search.py
+++ week5/Search/binary_search.py
@@ -1,26 +1,40 @@
-def recursive_binary_search(list, value):
-    while len(list) != 0:
-        midway = len(list)//2
-        if list[midway] == value:
+def binary_search_recursive(list, value, start=0, end=None):
+    if end is None:
+        end = len(list) - 1
+    if start > end:
+        return False
+
+    mid = (start + end) // 2
+    if value == list[mid]:
+        return True
+    if value < list[mid]:
+        return binary_search_recursive(list, value, start, mid-1)
+    # value > list[mid]
+    return binary_search_recursive(list, value, mid+1, end)
+
+
+def binary_search_iterative(list, value):
+    start, end = 0, (len(list) - 1)
+    while start <= end:
+        mid = (start + end) // 2
+        if value == list[mid]:
             return True
-        elif list[midway] > value:
-            return recursive_binary_search(list, value)
-        elif list[midway] < value:
-            return recursive_binary_search(list, value)
-        else:
-            return False
+        if value < list[mid]:
+            end = mid - 1
+        else:  # value > list[mid]
+            start = mid + 1
 
-
-# def iterative_binary_search(list, value):
-#     start, end = 0, len(list) - 1
-#     while start <= end:
-#         mid = (start + end)//2
+    return False
 
 
 sampleInput1 = [7, 20, 26, 31, 40, 51, 55, 63, 74, 81]
 sampleInput2 = [7, 20, 26, 31, 40, 51, 55, 63, 74, 81]
 sampleInput3 = ["Alpha", "Beta", "Delta", "Epsilon", "Gamma", "Theta", "Zeta"]
 
-recursive_binary_search(sampleInput1, 31)
-recursive_binary_search(sampleInput2, 77)
-# recursive_binary_search(sampleInput3, "Delta")
+# print(binary_search_recursive(sampleInput1, 31))
+# print(binary_search_recursive(sampleInput2, 77))
+# print(binary_search_recursive(sampleInput3, "Delta"))
+
+print(binary_search_iterative(sampleInput1, 31))
+print(binary_search_iterative(sampleInput2, 77))
+print(binary_search_iterative(sampleInput3, "Delta"))
