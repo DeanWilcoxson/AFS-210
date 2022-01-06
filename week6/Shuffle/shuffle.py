7249b7e298c6ef1837472b4aa4968f080a25d9e4
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2022-01-05 21:18:14 -0800

    1/5/22

--- week6/Shuffle/shuffle.py
+++ week6/Shuffle/shuffle.py
@@ -1,15 +1,20 @@
 import random
+
+
 def shuffle(list, n):
     if len(list) <= 0:
         return None
     elif len(list) < 2 and len(list) > 0:
         return list
     elif len(list) >= 2:
-        for index in range(n-1,0,-1):
+        for index in range(n-1, 0, -1):
             randIndex = random.randint(0, index+1)
-            list[index],list[randIndex] = list[randIndex],list[index]
+            list[index], list[randIndex] = list[randIndex], list[index]
         return list
-        
+    else:
+        return None
+
+
 sampleInput = [7, 20, 26, 31, 40, 51, 55, 63, 74, 81]
 n = len(sampleInput)
 print("Before Shuffled: ")
