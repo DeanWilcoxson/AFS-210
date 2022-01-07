425c0d6c99d6011bc7a22bd5997ff221566fff65
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2022-01-07 10:11:55 -0800

    1/7/22

--- week6/Shuffle/shuffle.py
+++ week6/Shuffle/shuffle.py
@@ -1,27 +1,23 @@
 import random
-
-def shuffle(list, n):
+def shuffle(list):
     if len(list) <= 0:
         return None
-    elif len(list) < 2 and len(list) > 0:
-        return list
-    elif len(list) >= 2:
-        for index in range(n-1, 0, -1):
-            randIndex = random.randint(0, index+1)
-            list[index], list[randIndex] = list[randIndex], list[index]
+    elif len(list) == 1:
         return list
     else:
-        return None
+        for index in range(len(list)-1, 0, -1):
+            randIndex = random.randint(0, index)
+            list[index], list[randIndex] = list[randIndex], list[index]
+        return list
 
 sampleInput = [7, 20, 26, 31, 40, 51, 55, 63, 74, 81]
-n = len(sampleInput)
 print("Before Shuffled: ")
 print(sampleInput)
 print("After Shuffled: ")
-print(shuffle(sampleInput, n))
-print(shuffle(sampleInput, n))
-print(shuffle(sampleInput, n))
-print(shuffle(sampleInput, n))
-print(shuffle(sampleInput, n))
+print(shuffle(sampleInput))
+print(shuffle(sampleInput))
+print(shuffle(sampleInput))
+print(shuffle(sampleInput))
+print(shuffle(sampleInput ))
 
 # Time Complexity should be O(n)
